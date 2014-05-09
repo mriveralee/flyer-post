a6a744594d40f99d7a5c92acfef388fbe7b4cce1
Michael Rivera <mrivera.lee@gmail.com>
2014-05-09 02:07:43 -0400

    FB data sanitization, fixed footer, and using async for getting picture and event data, updated nav

--- controllers/events.js
+++ controllers/events.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 var graph = require('fbgraph');
 var User = require('../models/User');
 var Event = require('../models/Event');
+var async = require('async');
 
 
 /**
@@ -24,13 +25,13 @@ exports.getCreateEvent = function(req, res) {
 exports.postCreateEvent = function(req, res) {
   // Read FB url from the post and get all of the other information using the api calls.
   req.assert('fb_url', 'Facebook Event Url cannot be blank').notEmpty().isUrl();
-  req.assert('img_url', 'Event Image Url cannot be blank').notEmpty();
+  //req.assert('img_url', 'Event Image Url cannot be blank').notEmpty();
 
   if (!isImageUrl(req.body.img_url)) {
     req.body.img_url ='';
   }
 
-  req.assert('img_url', 'Event Image Url must be an image').notEmpty();
+  // req.assert('img_url', 'Event Image Url must be an image').notEmpty();
 
   var errors = req.validationErrors();
 
@@ -52,25 +53,31 @@ exports.postCreateEvent = function(req, res) {
 
   // Callback to render event page, if necessary
   var renderEventPage = function(err, eventData) {
-    if (err || !isFacebookEvent(eventData)) {
-      // Go to create events page if not an actual facebook event & flash error
+    if (err) {
       if (err.code === 11000) {
-        req.flash('errors', { msg: 'Facebook Event with that url already exists.' });
-      } else {
-        // Show an error for an invalid fb url
+        console.log(err);
         req.flash('errors',
-          [{ param: 'fbEventUrl',
-             msg: 'Invalid Facebook Event URL',
-             value: (eventData && eventData.fbId) ? eventData.fbId : null
-           }]);
+          { msg: 'This event already exists.' });
+        // Return to create page and show errors
+      } else {
+        req.flash('errors', { msg: err.msg });
       }
-
+      return res.redirect('/events/create');
+    } else if (!isFacebookEvent(eventData)) {
+      // Show an error for an invalid fb url
+      req.flash('errors',
+        [{ param: 'fbEventUrl',
+           msg: 'Invalid Facebook Event URL',
+           value: (eventData && eventData.fbId) ? eventData.fbId : null
+         }]
+       );
       // Redirect to the create page & show errows :)
       return res.redirect('/events/create');
     }
 
     // Otherwise show the newly created events page
-    res.redirect('/events/view/' + eventData.fbId);
+    req.flash('success', { msg: 'Event created.' });
+    return res.redirect('/events/view/' + eventData.fbId);
   };
 
   // Grab the FB access token
@@ -94,7 +101,8 @@ exports.deleteEvent = function(req, res, next) {
   Event.remove({ fbId: req.param.eventId, owner: req.user.id }, function(err) {
     if (err) return next(err);
     //req.logout();
-    res.redirect('/');
+    req.flash('success', { msg: 'Event Deleted.' });
+    return res.redirect('/');
   });
 
 };
@@ -118,50 +126,130 @@ exports.getEvent = function(req, res, next) {
     return res.render('events/view', {
       title: fbEvent.name + ' - FlyerPost',
       fbEvent: fbEvent,
+      useEventCover: false,
+      useEventPicture: true
     });
   });
 };
 
+
 //------------------------------------------------------------------------------
+/**
+ * Facebook Event Url Query Params
+ */
+var FB_EVENT_PARAMS = '/?fields=cover,id,name,owner,description,privacy,start_time,end_time,location,is_date_only,ticket_uri,venue,picture';
+var FB_EVENT_PICTURE_EDGE = '/picture?redirect=0&type=large'
+//------------------------------------------------------------------------------
+
+
 /**
  * Retrieves a Facebook event from the FB API
  *
  **/
-
 var retrieveFacebookEvent = function(token, fbEventId, eventImgUrl, next) {
   if (!token) {
-    next(err);
+    next({msg: 'You must be signed in to Facebook!', code: 9999});
     return;
   }
-  // Set the access token
-  graph.setAccessToken(token.accessToken);
-  graph.get(fbEventId, function(err, eventData) {
-    if (err) return next(err);
-    // Store the Event in our Database
-    eventData.fbId = eventData.id;
-    delete eventData.id;
-    var dbEvent = new Event(eventData);
-    dbEvent.img_url = eventImgUrl;
+  async.parallel({
+    event: function(callback) {
+      // Set the access token
+      graph.setAccessToken(token.accessToken);
+      graph.get(fbEventId + FB_EVENT_PARAMS, function(err, eventData) {
+        if (err) return callback(err);
+        if (!isFacebookEvent(eventData)) {
+          err = {msg: 'Invalid Facebook Event', code: 9999};
+          return callback(err);
+        }
+        return callback(null, eventData);
+      });
+    },
+    picture: function(callback) {
+      // Set the access token
+      graph.setAccessToken(token.accessToken);
+      graph.get(fbEventId + FB_EVENT_PICTURE_EDGE, function(err, pictureData) {
+        if (err) return callback(err);
+        // Store the Event in our Database
+        var pic = (pictureData && pictureData.data && pictureData.data.url)
+          ? pictureData.data.url : '';
+        return callback(null, pictureData);
+      });
+    }
+  }, function(err, results) {
+    if (err) {
+      console.log(err, 'In async cb');
+      return next(err);
+    }
+    var evData = sanitizeFacebookEvent(results.event);
+    var picData = sanitizeFacebookEventPhoto(results.picture);
+    evData.picture = picData;
 
-    dbEvent.save(function(err) {
-      if (err) {
-        return next(err);
+    // console.log(eventData);
+    var dbEvent = new Event(evData);
+
+    dbEvent.save(function(nErr) {
+      if (nErr) {
+        console.log(nErr, 'db Save');
+        return next(nErr);
       }
-
       // Now render the page
-      return next(null, eventData);
+      return next(null, dbEvent);
     });
+
+    //dbEvent.picture = picData;
+
+    //dbEvent.save
+
+    console.log (dbEvent);
+    //dbEvent.img_url = eventImgUrl;
+    // Save in DB
+
   });
 };
+
+
+/**
+ * Sanitize FB Event Data
+ *
+ **/
+ var sanitizeFacebookEvent = function(fbEvent) {
+   var ev = {};
+   ev.cover = fbEvent.cover;
+   ev.fbId = fbEvent.id;
+   ev.name = fbEvent.name;
+   ev.owner = fbEvent.owner;
+   ev.description = fbEvent.description;
+   ev.privacy = fbEvent.privacy;
+   ev.start_time = fbEvent.start_time;
+   ev.end_time = fbEvent.end_time;
+   ev.location = fbEvent.location;
+   ev.is_date_only = fbEvent.is_date_only;
+   ev.ticket_uri = fbEvent.ticket_uri;
+   ev.venue = fbEvent.venue;
+   return ev;
+ };
+
+ /**
+  * Sanitize FB Event Photo
+  *
+  **/
+var sanitizeFacebookEventPhoto = function(pictureData) {
+  var photo = (pictureData && pictureData.data && pictureData.data.url)
+    ? pictureData.data.url : '';
+
+  return photo;
+};
+
+
 
 /**
  * Test if a set of data is an FB Event data
  *
  **/
-
 var isFacebookEvent = function(eventData) {
   if (!eventData) return false;
-  return eventData.owner && eventData.description && eventData.name;
+  // console.log(eventData.privacy);
+  return eventData.owner && eventData.description && eventData.name && eventData.privacy == 'OPEN';
 };
 
 /**
@@ -178,8 +266,8 @@ var isFacebookEvent = function(eventData) {
  *
  **/
 var clearEventModel = function() {
-  Event.remove({}, function(err) {
-    console.log('Event Collection removed');
+  Event.remove({}, function(err, rm) {
+    console.log('Event Collection removed', rm);
   });
 };
 
--- models/Event.js
+++ models/Event.js
@@ -1,28 +1,33 @@
 var mongoose = require('mongoose');
 
-var  fbBaseEventUrl = 'http://www.facebook.com/'
+var fbBaseEventUrl = 'http://www.facebook.com/';
 
 var eventSchema = new mongoose.Schema({
-  fbId: { type: String, unique: true },
-  name: String,
-  description: String,
-  privacy: String,
-  owner: Object,
-  event_photo: String,
-  cover_photo: String,
-  start_time: String,
-  end_time: String,
-  location: String,
-  is_date_only: Boolean,
-  ticket_uri: String,
-  updated_time: String,
-  timezone: String,
-  venue: String,
-  attending: Array,
-  feed: Array,
-  photos: Array,
-  img_url: String,
-  tags: Array
+  fbId: { type: String, unique: true},
+
+  name: {type: String, default: ''},
+  description: {type: String, default: ''},
+  privacy: {type: String, default: ''},
+  owner: {type: Object, default: {}},
+  picture: {type: String, default: ''},
+  cover: {type: Object, default: {}},
+  start_time: {type: String, default: ''},
+  end_time: {type: String, default: ''},
+
+  location: {type: String, default: ''},
+  is_date_only: {type: Boolean, default: true},
+  ticket_uri: {type: String, default: ''},
+  updated_time: {type: String, default: ''},
+  timezone: {type: String, default: ''},
+
+  venue: {type: String, default: ''},
+  attending: {type: Array, default: []},
+  feed: {type: Array, default: []},
+  photos: {type: Array, default: []},
+
+  img_url: {type: String, default: ''},
+
+  tags: {type: Array, default: []},
 });
 
 // TODO: Get event details from the FB Events API
@@ -36,4 +41,4 @@ var eventSchema = new mongoose.Schema({
  */
 
 
-module.exports = mongoose.model('Event', eventSchema);
+module.exports = mongoose.model('FPEvents', eventSchema);
